1a51872ada8ed7f85ae3b1fb5a7cd3ad1fe34e5a
Ilya <ifed@tuta.io>
2020-03-23 16:52:49 +0300

    test: add onliner autotest
    - Add test config (login + password)
    - Add autotest

---
+++ src/main/java/pages/SectionPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import driver.Browser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class SectionPage {
+
+    private WebElement sectionTitle = Browser.getDriver().findElement(By.xpath("//div[@class='schema-header']//*[@class='schema-header__title']"));
+
+
+    public String getSectionTitleText() {
+        return sectionTitle.getText();
+    }
+}
